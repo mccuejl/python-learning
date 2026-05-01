654c85495a72fca7fe1b853cfa146108ec5962d1
Jamie McCue <mccuejl@gmail.com>
2026-05-01 14:39:26 -0600

    Day 1 - WIP:print statements basics

---
+++ 01_learning_resources/udemy_100_days_of_code/day_1/task1.py
@@ -0,0 +1,12 @@
+#Day 1 of 100 Days of Code Practice File - Hello World and String Concatenation 
+
+# Classic Hello World program in Python
+print("Hello, World!\n")
+
+# Hello World using \n for new line
+print("Hello World!\nWhat is your name?\n")
+
+# Practicing string concatenation
+print("Hello " + "Jamie!")
+print("Hello" + " Jamie!")
+print("Hello" + " " +"Jamie!")
