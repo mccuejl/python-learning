1c990be993344a27f96613bc40f370bb894c9a44
Jamie McCue <mccuejl@gmail.com>
2026-05-09 11:20:32 -0600

    Day 2 lessons on data types and type errors

---
+++ 01_learning_resources/udemy_100_days_of_code/day_2/task1.py
@@ -0,0 +1,20 @@
+# Day 2 of 100 Days of Code Practice File - Data Types
+
+# Subscripting to pull out individual characters from a string
+print("Hello"[3])  
+
+# Subscripting with negative numbers to pull out characters from the end of the string
+print("Hello"[-4])
+
+# Integer is a whole number without a decimal point
+print(123 + 456)
+
+# Large integers - can use a _ in between a large number to make it more readable, but it doesn't affect the value of the number & Python will ignore the _
+print(987_654_321)
+
+# Float is a number with a decimal point (think that the . is a floating point)
+print(9.99)
+
+# Boolean is a data type that can only be True or False (always a captial T or F)
+print(True)
+print(False)
