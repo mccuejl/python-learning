dfe764c5eb5c8b196477b1bbc38473dda5e52e3a
Jamie McCue <mccuejl@gmail.com>
2026-05-24 14:03:04 -0600

    Day 03 - if/else, conditional operators, odd/even number code challenge

---
+++ 01_learning_resources/udemy_100_days_of_code/day_3/task1.py
@@ -0,0 +1,11 @@
+# Day 3 of 100 Days of Code Practice File - if/else and conditional operators
+
+# Condition check with if/else
+print("Welcome to the world's fastest rollercoaster!")
+height = int(input("What is your height in inches? "))
+
+if height >= 48:
+    print("You are tall enough to ride the rollercoaster!🎉")
+else:
+    print("Sorry, you are not tall enough. Please come back when you are taller to ride the rollercoaster!")
+
---
+++ 01_learning_resources/udemy_100_days_of_code/day_3/task2.py
@@ -0,0 +1,12 @@
+# Day 3 of 100 Days of Code Practice File - code challenge 
+
+# Check if the number input is odd or even using the modulo operator and conditional checks
+print("Welcome to the odd/even number checker.")
+number_to_check = int(input("Please enter a number to check whether it is an odd or even number. \n"))
+
+if number_to_check % 2 == 0:
+    print("You entered an even number.")
+else:
+    print("You entered an odd number.")
+
+    
